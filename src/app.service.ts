b81c7864322b5252bf86197921989ce0ce72e66e
khaicq <44726195+khaicq@users.noreply.github.com>
2024-11-19 15:49:34 +0700

    test cicd

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "Hello World!";
+  }
+
+  getInformation(): string {
+    return "Update on 19 Nov at 15:48";
   }
 }
