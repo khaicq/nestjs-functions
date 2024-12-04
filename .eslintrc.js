67295781372056ac2389f6ce889ff651536ece3f
khaicq <44726195+khaicq@users.noreply.github.com>
2024-12-04 16:43:22 +0700

    update build files

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,16 +18,13 @@ module.exports = {
     sourceType: "module",
   },
   ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
+    "/dist/**/*", // Ignore built files.
     "/generated/**/*", // Ignore generated files.
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
-    "quotes": ["error", "double"],
+    quotes: ["error", "double"],
     "import/no-unresolved": 0,
-    "indent": ["error", 2],
+    indent: ["error", 2],
   },
 };
